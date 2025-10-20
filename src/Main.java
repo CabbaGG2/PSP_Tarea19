f77c6aa7e557d7e7790c606c7330d5fe0508f65a
Jose Camara <Jos.95camara@gmail.com>
2025-10-20 20:31:34 +0200

    mejora de interfaz.

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,37 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Buenas noches, programadores de pacotilla");
         System.out.println("Estamos aquí reunidos para seguir en este arduo camino de aprendizaje sobre Java");
-        System.out.println("Esta noche vamos a realizar un contador de vocales, asi que venga ingresa una frase toda chula aqui abajo:");
-        ContadorVocales cv = new ContadorVocales();
-        Scanner sc = new Scanner(System.in);
-        String frase = sc.nextLine();
-        Hilo hiloA = new Hilo("A", frase, cv);
-        Hilo hiloE = new Hilo("E", frase, cv);
-        Hilo hiloI = new Hilo("I", frase, cv);
-        Hilo hiloO = new Hilo("O", frase, cv);
-        Hilo hiloU = new Hilo("U", frase, cv);
-        hiloA.start();
-        hiloE.start();
-        hiloI.start();
-        hiloO.start();
-        hiloU.start();
-        try {
-            hiloA.join();
-            hiloE.join();
-            hiloI.join();
-            hiloO.join();
-            hiloU.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("El total de vocales es: " + cv.getContadorVocales());
-
+        String frase;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Escribe una frase para contar sus vocales o 'salir' para terminar el programa.");
+            frase = sc.nextLine();
+            if (frase.equals("salir")) {
+                System.out.printf("Finalizando programa.");
+                break;
+            }
+            ContadorVocales cv = new ContadorVocales();
+            Hilo hiloA = new Hilo("A", frase, cv);
+            Hilo hiloE = new Hilo("E", frase, cv);
+            Hilo hiloI = new Hilo("I", frase, cv);
+            Hilo hiloO = new Hilo("O", frase, cv);
+            Hilo hiloU = new Hilo("U", frase, cv);
+            hiloA.start();
+            hiloE.start();
+            hiloI.start();
+            hiloO.start();
+            hiloU.start();
+            try {
+                hiloA.join();
+                hiloE.join();
+                hiloI.join();
+                hiloO.join();
+                hiloU.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("El total de vocales es: " + cv.getContadorVocales());
+        } while (true);
     }
 }
 
